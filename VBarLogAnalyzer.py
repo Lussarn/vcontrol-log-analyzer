d63228e1e2888fd4cf501e0eb76e305af04a4424
Linus Larsson <linus.larsson@verajohn.com>
2015-07-01 08:43:58 +0200

    removed temporary vbar path

--- VBarLogAnalyzer.py
+++ VBarLogAnalyzer.py
@@ -298,7 +298,7 @@ class Analyzer:
 					continue
 
 	def _find_vcontrol_path(self):
-		return '/Users/linus/vcontrol'
+#		return '/Users/linus/vcontrol'
 		if 'linux' in sys.platform:
 #			return "/tmp";
 			drives=subprocess.Popen('mount', shell=True, stdout=subprocess.PIPE)
